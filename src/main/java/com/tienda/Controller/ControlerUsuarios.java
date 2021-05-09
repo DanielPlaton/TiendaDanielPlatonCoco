c31d2d50bb775590efb67552b7ac5b979e36b1db
DanielPlaton <dcp0495@gmail.com>
2021-05-09 19:11:53 +0200

    login bootstrap

--- src/main/java/com/tienda/Controller/ControlerUsuarios.java
+++ src/main/java/com/tienda/Controller/ControlerUsuarios.java
@@ -3,16 +3,15 @@ package com.tienda.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.tienda.modelo.Productos;
+
 import com.tienda.modelo.Usuarios;
-import com.tienda.services.ProductoServices;
+
 import com.tienda.services.UsuarioServices;
 
 
